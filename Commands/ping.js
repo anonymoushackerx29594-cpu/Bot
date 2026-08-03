7033455229b21a09debd8b9a7d28de8c60a6fce6
anonymoushackerx29594-cpu <anonymoushackerx29594@gmail.com>
2026-08-03 22:34:32 +0000

    Update ping.js

--- Commands/ping.js
+++ Commands/ping.js
@@ -1,14 +1,31 @@
 module.exports = {
     name: "ping",
+    description: "Affiche la latence du bot",
 
     async execute(sock, msg, args) {
+        const groupe = msg.key.remoteJid;
+        const start = Date.now();
 
         await sock.sendMessage(
-            msg.key.remoteJid,
+            groupe,
             {
-                text: "🏓 Pong ! king Bot est en ligne."
+                text: "📡 Pinging..."
+            },
+            {
+                quoted: msg
             }
         );
 
+        const latency = Date.now() - start;
+
+        await sock.sendMessage(
+            groupe,
+            {
+                text: `🚀 King Network\n\n⚡ Latence : ${latency} ms`
+            },
+            {
+                quoted: msg
+            }
+        );
     }
 };
